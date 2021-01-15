476f1340d41a2d08d664319fad04df188cd9b449
minwk <1294608448@qq.com>
2021-01-15 09:43:36 +0800

    fix office2016提示修复/xl/worksheets/sheet1.xml部分的格式问题

--- src/main/java/com/ibiz/excel/picture/support/module/Sheet1.java
+++ src/main/java/com/ibiz/excel/picture/support/module/Sheet1.java
@@ -14,8 +14,8 @@ public class Sheet1 {
     @AutoFile(subDir = "worksheets", fileName = "sheet1.xml", alias = Alias.SHEET1,
             xmlEnd = AutoXmlHeadEndContent.SHEET1_END)
     String content = "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" " +
-            "xmlns:xdr=\"http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing\" xmlns:x14=\"http://schemas.microsoft.com/office/spreadsheetml/2009/9/main\" " +
-            "xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\"><sheetPr/><dimension ref=\"A1:AG8\"/><sheetViews><sheetView tabSelected=\"1\" workbookViewId=\"0\">" +
-            "<selection activeCell=\"D3\" sqref=\"D3\"/></sheetView></sheetViews><sheetFormatPr defaultColWidth=\"20\" defaultRowHeight=\"130\" outlineLevelRow=\"10\"/><cols>" +
-            "<col min=\"1\" max=\"4\" width=\"23.5\" customWidth=\"20\"/></cols><sheetData>";
+            "xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\" mc:Ignorable=\"x14ac\" xmlns:x14ac=\"http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac\" " +
+            "><dimension ref=\"A1:L2\"/><sheetViews><sheetView tabSelected=\"1\" topLeftCell=\"E1\" workbookViewId=\"0\">" +
+            "<selection activeCell=\"E19\" sqref=\"E19\"/></sheetView></sheetViews><sheetFormatPr defaultColWidth=\"20\" defaultRowHeight=\"13.5\" x14ac:dyDescent=\"0.15\"/><cols>" +
+            "<col min=\"1\" max=\"4\" width=\"23.5\"/></cols><sheetData>";
 }
