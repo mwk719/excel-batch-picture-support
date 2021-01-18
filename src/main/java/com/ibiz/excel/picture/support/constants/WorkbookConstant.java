91a599dcfbd1adedb9635ac78496616e69225a5e
minwk <1294608448@qq.com>
2021-01-18 16:33:55 +0800

    fix 斜杠路径

--- src/main/java/com/ibiz/excel/picture/support/constants/WorkbookConstant.java
+++ src/main/java/com/ibiz/excel/picture/support/constants/WorkbookConstant.java
@@ -6,7 +6,7 @@ package com.ibiz.excel.picture.support.constants;
  */
 public class WorkbookConstant {
     public static final String AUTO_DIR = System.getProperty("java.io.tmpdir");
-    public static final String FILE_SEPARATOR = "\\";
+    public static final String FILE_SEPARATOR = "/";
     public static final String DEST_FILE_NAME_SUFFIX = ".xlsx";
     public static final String MEDIA_IMAGE_TYPE = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image";
     /**有图片的行高设置为100*/
