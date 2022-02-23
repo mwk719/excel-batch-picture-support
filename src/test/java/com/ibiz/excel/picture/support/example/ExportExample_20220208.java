24fec7ef7999df9123af8e8127e16ce9cde75232
minwk <1294608448@qq.com>
2022-02-23 14:05:25 +0800

    add 完善注释

--- src/test/java/com/ibiz/excel/picture/support/example/ExportExample_20220208.java
+++ src/test/java/com/ibiz/excel/picture/support/example/ExportExample_20220208.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 动态标题excel导出
+ * 动态配置表头excel导出
  *
  * @author MinWeikai
  * @date 2022/2/8 14:49
@@ -25,33 +25,36 @@ public class ExportExample_20220208 extends BaseJunitTest {
 
     @Test
     public void export() {
+        // 模拟需要导出的数据集合
         List<Student> students = new ArrayList<>();
         students.add(new Student("李四", 16, null, null, 0));
         students.add(new Student("张三", 17, null, getPictures(5), 1));
         students.add(new Student("王五", 15, IMG_PATH_1, null, 2));
 
-        Workbook workBook = Workbook.getInstance(100);
-        Sheet sheet = workBook.createSheet("测试");
-
+        // 配置导出excel的表头、顺序、对应导出的数据集合的字段、是否是图片、单元格宽度等
         List<BizExcelRel> excels = new ArrayList<>();
         excels.add(new BizExcelRel("姓名", "name", 2));
         excels.add(new BizExcelRel("年龄", "age", 3));
         excels.add(new BizExcelRel("表现", "performance", 4));
         excels.add(new BizExcelRel("头像", "headPicture", 5, true, 20));
         excels.add(new BizExcelRel("相册", "album", 6, true));
-        // 样式
-        CellStyle cellStyle = new CellStyle(0, "F0F0F0");
 
+        // 创建excel
+        Workbook workBook = Workbook.getInstance(100);
+        Sheet sheet = workBook.createSheet("测试");
+        // 创建样式
+        CellStyle cellStyle = new CellStyle(0, "F0F0F0");
+        // 创建数据字典
         Map<String, String> performanceMap = new HashMap<>();
         performanceMap.put("0", "一般");
         performanceMap.put("1", "良好");
         performanceMap.put("2", "优秀");
 
         // 构建sheet
-        ExcelTableProcessor.build(sheet)
+        ExcelTableProcessor.sheet(sheet)
                 // 添加样式
                 .addCellStyle(cellStyle)
-                // 添加数据字典
+                // 添加对应属性字段的数据字典
                 .registryEnumMap("performance", performanceMap)
                 // 构建excel
                 .buildExcel(excels, students);
